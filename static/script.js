797a87bd94910684b3ed06275f34595ea6fde963
Aasees <aaseesbadesha13@gmail.com>
2025-05-28 16:45:50 -0400

    updated colour scheme of website

--- static/script.js
+++ static/script.js
@@ -1,32 +1,35 @@
 function drawWinLine(cells) {
     line = document.getElementById('line');
 
-    // default settings
-    line.style.transform = "none";
-
     // horizontal
     if (JSON.stringify(cells) == JSON.stringify([3,4,5])) {}
     else if (JSON.stringify(cells) == JSON.stringify([0, 1, 2])) {
-        line.style.top = 60 +'px';
+        line.style.top = 50 +'px';
     }
     else if (JSON.stringify(cells) == JSON.stringify([6, 7, 8])) {
-        line.style.top = 240 +'px';
+        line.style.top = 230 +'px';
     }
     // diagonal
     else if (JSON.stringify(cells) == JSON.stringify([0, 4, 8])) {
         line.style.transform = "rotate(225deg)";
+        line.style.width = '110%';
+        line.style.top = 138 + 'px';
+        line.style.left = -14 + 'px';
     }
     else if (JSON.stringify(cells) == JSON.stringify([2, 4, 6])){
         line.style.transform = "rotate(135deg)";
+        line.style.width = '110%';
+        line.style.top = 138 + 'px';
+        line.style.left = -14 + 'px';
     }   
     // vertical
     else {
         line.style.transform = "rotate(90deg)";
         if (JSON.stringify(cells) == JSON.stringify([0, 3, 6])) {
-            line.style.left = -60 +'px';
+            line.style.left = -75 +'px';
         }
         else if (JSON.stringify(cells) == JSON.stringify([2, 5, 8])) {
-            line.style.left = 125 +'px';
+            line.style.left = 107 +'px';
         }
     }
     
